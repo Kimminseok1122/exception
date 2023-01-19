f2a55a19c1722ea56b75284a6044ed62503b7faf
EOS <wisedom2@gmail.com>
2023-01-19 17:42:07 +0900

    개발의 개 연도의 연

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -39,19 +39,19 @@ public class ErrorPageController {
         return "error-page/500";
     }
 
-    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
-
-        log.info("API errorPage 500");
-
-        Map<String, Object> result = new HashMap<>();
-        Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
-        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
-        result.put("message", ex.getMessage());
-
-        Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
-    }
+//    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+//
+//        log.info("API errorPage 500");
+//
+//        Map<String, Object> result = new HashMap<>();
+//        Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+//        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+//        result.put("message", ex.getMessage());
+//
+//        Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+//    }
 
 
     private void printErrorInfo(HttpServletRequest request) {
